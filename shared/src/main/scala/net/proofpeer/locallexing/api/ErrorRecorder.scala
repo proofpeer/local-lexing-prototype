b1090ae0faa2af2f03509a553a37d3042aca7730
Steven Obua <steven.obua@gmail.com>
2017-04-01 16:09:27 +0100

    Implemented ErrorRecorder.toString

--- shared/src/main/scala/net/proofpeer/locallexing/api/ErrorRecorder.scala
+++ shared/src/main/scala/net/proofpeer/locallexing/api/ErrorRecorder.scala
@@ -1,5 +1,7 @@
 package net.proofpeer.locallexing.api
 
+import net.proofpeer.locallexing.utils.StringUtils
+
 trait ErrorRecorder {
   def record(msg : ErrorMessage)
 }
@@ -15,5 +17,28 @@ abstract class ErrorMessage(msg : String, args : Vector[Annotated]) {
       case _ => arg.toString
     }
     "'" + s + "'"
+  }
+  override def toString : String = {
+    val size = msg.size
+    var s : String = ""
+    var i : Int = 0
+    do {
+      val pos = msg.indexOf("%", i)
+      if (pos < 0) {
+        return s + msg.substring(i)
+      } else {
+        if (pos + 1 < size  && StringUtils.isASCIIDigit(msg(pos + 1))) {
+          var j = pos + 2
+          while (j < size && StringUtils.isASCIIDigit(msg(j))) j += 1
+          val argIndex = msg.substring(pos + 1, j).toInt
+          s = s + msg.substring(i, pos) + asString(args(argIndex))
+          i = j
+        } else {
+          s = s + msg.substring(i, pos + 1)
+          i = pos + 1
+        }
+      }
+    } while (true)
+    throw new RuntimeException("ErrorMessage.toString: internal error")
   }
 }
